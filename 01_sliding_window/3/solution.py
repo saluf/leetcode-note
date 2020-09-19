f3408d0345c50db3e063477c388ad76a157f9b9e
SzaAn Lu <szuan.lu@gmail.com>
2020-09-19 10:01:21 +0800

    Update no.03 python solution

--- 01_sliding_window/3/solution.py
+++ 01_sliding_window/3/solution.py
@@ -9,6 +9,8 @@ note:
 time complexity: O(n)
 space complexity: O(1)
 
+Runtime: 52 ms, faster than 92.13%
+Memory Usage: 14.1 MB, less than 20.32%
 """
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
@@ -20,9 +22,9 @@ class Solution:
         
         for r, char in enumerate(s):
             
-            if char in char_map and l < char_map[char] + 1:
-                # the repeated char is in the window
-                # shif l to next position to ensure the char is not repeated
+            if char in char_map and char_map[char] >= l:
+                # the duplicated char is in the window
+                # shif l to next position to ensure the char is not duplicated
                 l = char_map[char] + 1
             
             # record last position
